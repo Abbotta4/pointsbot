4797c29c06fc3ff605c0b0472863900dca2d2810
Andrew <abbotta4@gmail.com>
2017-10-23 02:19:43 -0400

    initial implementation of the /addpoint and /rmpoint commands

--- pointsbot.py
+++ pointsbot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 import logging,ConfigParser,io,sqlite3
-from telegram.ext import Updater,MessageHandler,CommandHandler,Filters,BaseFilter
+from telegram.ext import Updater,CommandHandler
 
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 
@@ -18,26 +18,40 @@ updater = Updater(token = config.get('telegram', 'token'))
 dispatcher = updater.dispatcher
 username = config.get('telegram', 'username')
 
-class FilterUsername(BaseFilter):
-    def filter(self, message):
-        return username in message.text
-filter_username = FilterUsername()
-
-def addpoint(bot, update):
-    connfile = update.message.chat_id + '.db'
+def editdb(bot, update):
+    connfile = 'db/' + update.message.chat_id + '.db'
     conn = sqlite3.connect(connfile)
     cursor = conn.cursor()
-    if cursor.execute("""SELECT name FROM sqlite_master WHERE type='table'""").fetchall().empty():
-        pass #create table
-    
+    cursor.execute('''CREATE TABLE IF NOT EXISTS points (username TEXT PRIMARY KEY, adds INT, rms INT, total INT)''')
+    entities = update.message.parse_entities()
+    usernames = []
+    for e in entities.keys():
+        if e == 'mention':
+            usernames.append(entities[e])
+        if e == 'text_mention':
+            response = "I'm not touching " + entities[e] + "'s points, ya turkey, that goober needs a username!"
+            bot.send_message(chat_id = update.message.chat_id, text = response)
+            
+    for u in usernames:
+        cursor.execute("""SELECT adds, rms, total FROM points WHERE username = %s""", (u, ))
+        points = cursor.fetchone()
+        adds = points[0]
+        rms = points[1]
+        total = points[2]
+        if entities['bot_command'] == 'addpoint':        
+            adds = adds + 1
+        if entities['bot_command'] == 'rmpoint':
+            rms = rms + 1
+        total = adds + rms
+        cursor.execute("""UPDATE points SET adds = %d, total = %d WHERE username = %s""", (adds, total, u))
+        cursor.commit()
+        response = u + ' - ' + '+' + adds + '/-' + rms + ' total: ' + total
+        bot.send_message(chat_id = update.message.chat_id, text = response)
 
-def rmpoint(bot, update):
-    pass
-    bot.send_message(chat_id = update.message.chat_id, text = response)
+    cursor.close()
 
-response_handler = MessageHandler(filter_username, respond)
-dispatcher.add_handler(CommandHandler('addpoint', addpoint))
-dispatcher.add_handler(CommandHandler('rmpoint', rmpoint))
+dispatcher.add_handler(CommandHandler('addpoint', editdb))
+dispatcher.add_handler(CommandHandler('rmpoint', editdb))
 
 updater.start_polling()
 updater.idle()
