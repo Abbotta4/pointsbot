d3cd72f0859f81755fc7619b3fde911ed4ce8c6b
Drew Abbott <abbotta4@gmail.com>
2018-03-07 21:02:59 -0500

    added non-functioning votepoint and run_async decorators

--- pointsbot.py
+++ pointsbot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
-import logging,ConfigParser,io,sqlite3
+import logging,ConfigParser,io,sqlite3,time
 from telegram.ext import Updater,CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext.dispatcher import run_async
 
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 
@@ -31,8 +33,29 @@ class db_cursor:
         self.conn.commit()
         self.cursor.close()
 
+def get_users(bot, update, cursor):
+    entities = update.message.parse_entities()
+    usernames = []
+    for e in entities.keys():
+        if e.type == 'mention' and entities[e].lower() not in [x.lower() for x in usernames]:
+            usernames.append(entities[e])
+        if e.type == 'text_mention':
+            response = "I'm not touching " + entities[e] + "'s points, ya turkey, that goober needs a username!"
+            bot.send_message(chat_id = update.message.chat_id, text = response)
+            usernames.append(None)
+
+    if update.message.reply_to_message:
+        reply_user = '@' + update.message.reply_to_message.from_user.username
+        if reply_user.lower() not in [x.lower() for x in usernames]:
+            usernames.append(reply_user)
+
+    if not usernames:
+        bot.send_message(chat_id = update.message.chat_id, text = 'No username(s) sent.')
+
+@run_async
 def addrmpoint(bot, update):
     with db_cursor(update) as cursor:
+        #usernames = get_users(bot, update, cursor)
         entities = update.message.parse_entities()
         usernames = []
         for e in entities.keys():
@@ -68,6 +91,7 @@ def addrmpoint(bot, update):
                 response = u + ' - ' + '+' + str(adds) + '/-' + str(rms) + ' total: ' + str(total)
                 bot.send_message(chat_id = update.message.chat_id, text = response)
 
+@run_async
 def top10(bot, update):
     with db_cursor(update) as cursor:
         cursor.execute('''SELECT * FROM points ORDER BY total ASC''')
@@ -79,9 +103,64 @@ def top10(bot, update):
         for username, x in zip(top10, range (1, 11)):
             response = response + str(x) + '. ' + username[0] + ' - ' + '+' + str(username[1]) + '/-' + str(username[2]) + ' total: ' + str(username[3]) + '\n'
         bot.send_message(chat_id = update.message.chat_id, text = response)
+@run_async
+def votepoint(bot, update):
+    with db_cursor(update) as cursor:
+        entities = update.message.parse_entities()
+        usernames = []
+        for e in entities.keys():
+            if e.type == 'mention' and entities[e].lower() not in [x.lower() for x in usernames]:
+                usernames.append(entities[e])
+            if e.type == 'text_mention':
+                response = "I'm not touching " + entities[e] + "'s points, ya turkey, that goober needs a username!"
+                bot.send_message(chat_id = update.message.chat_id, text = response)
+                usernames.append(None)
+
+        if update.message.reply_to_message:
+            reply_user = '@' + update.message.reply_to_message.from_user.username
+            if reply_user.lower() not in [x.lower() for x in usernames]:
+                usernames.append(reply_user)
+
+        if not usernames:
+            bot.send_message(chat_id = update.message.chat_id, text = 'No username(s) sent.')
+
+        for u in usernames:
+            if u is not None:
+                cursor.execute("""SELECT adds, rms FROM points WHERE username = ?""", (u.lower(), ))
+                points = cursor.fetchone()
+                if points is None:
+                    points = [0, 0, 0]
+                adds = points[0]
+                rms = points[1]
+                button_list = [[InlineKeyboardButton("add", callback_data='add'), InlineKeyboardButton("rm", callback_data='rm')]]
+                reply_markup = InlineKeyboardMarkup(button_list)
+                votemessage = bot.send_message(chat_id = update.message.chat_id, text = "Should this be add or rm? 5m00s", reply_markup = reply_markup)
+
+                def callback_countdown(bot, message, t):
+                    message.edit_text(text="Should this be add or rm? {}".format(str(t//60) + "m" + str(t%60) + "s"))
+                    
+                for t in range(299, 0, -1):
+                    time.sleep(1)
+                    callback_countdown(bot, votemessage, t) 
+                
+#                for t in range(300, 0, -1):
+ #                   time.sleep(1)
+  #                  votemessage.edit_text(text="Should this be add or rm? {}".format(str(t//60) + "m" + str(t%60) + "s"))
+   #                 query = update.callback_query
+    #                if query:
+     #                   query = query.data
+      #              votemessage.edit_reply_markup(reply_markup = reply_markup)
+                #if update.message.text.startswith('/add'):
+                #    adds = adds + 1
+                #else: #message.text.startswith('/rm'):
+                #    rms = rms + 1
+                total = adds - rms
+                cursor.execute("""REPLACE INTO points (username, adds, rms, total) VALUES (?, ?, ?, ?)""", (u.lower(), adds, rms, total))
+                response = u + ' - ' + '+' + str(adds) + '/-' + str(rms) + ' total: ' + str(total)
+                bot.send_message(chat_id = update.message.chat_id, text = response)
 
 dispatcher.add_handler(CommandHandler(['addpoint', 'rmpoint'], addrmpoint))
 dispatcher.add_handler(CommandHandler(['top10'], top10))
-
+dispatcher.add_handler(CommandHandler(['votepoint'], votepoint))
 updater.start_polling()
 updater.idle()
